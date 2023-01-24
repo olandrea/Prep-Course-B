708234ef28d618173ced1e720ec5eb811af8d65a
olandrea <andreaochoaispuru@gmail.com>
2023-01-24 04:05:28 -0300

    homework 01, 02, 03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,81 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0; 
+  for (let i = 1; i <= 10; i++) {
+        suma = suma + i;
+  }
+    return suma; 
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var nuevoArray = [];
+  var indice = 0;
+  //for ( let i = 0; i < array.length; i ++){
+    while (indice < array.length) {
+     if ( array[indice] % 2 === 0 ){
+      nuevoArray.push(array[indice]);
+    }
+    indice ++;
+  }
+  return nuevoArray;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+ // var cuadrado = 0; 
+ // var num = 0;
+ // var nuevoArray = [];
+ // for ( let i = 0; i < array.length; i ++){
+  //    num = array[i];
+  //    cuadrado = Math.pow(num,2);
+  //    nuevoArray.push(cuadrado);
+  //}
+  //return nuevoArray;
+
+  //____________________________________
+  //return array.map (function (num){
+  //  return Math.pow (num, 2);
+  //})
+
+  //----------------------------------
+
+  var alCuadrado = function (num){
+    return Math.pow (num, 2);
+  }
+
+  var resultado = array.map (alCuadrado);
+  return resultado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+ // var suma = 0;
+ // var num = 0;
+ // for ( let i = 0; i < array.length; i ++){
+ //   num = array[i];
+ //   suma = suma + num; 
+ // }
+ // return suma;
+
+ var resultado = array.reduce(function (acumulador, num) {
+   return acumulador + num;
+ }, 0);
+ return resultado;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var digitosStr = num + "";
+  return digitosStr.length;
 }
   
   
